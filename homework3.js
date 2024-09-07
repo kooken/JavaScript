827508f06045fc1cf62b145f52b938b0dbc98b78
mariasazhina <msazhina23@gmail.com>
2024-09-07 22:45:59 +0300

    codewars hw

--- homework3.js
+++ homework3.js
@@ -72,3 +72,42 @@ for (let i = 0; i < encrypted.length; i += 3){
 	decrypted = decrypted + encrypted[i];
 }
 console.log(decrypted);
+
+// Tasks from CodeWars
+
+// Implement a function called multiply, which takes two numbers and returns their product
+const multiply = (number1, number2) => {
+	return number1 * number2;
+};
+
+/*
+You are given two interior angles (in degrees) of a triangle.
+Write a function to return the 3rd.
+Note: only positive integers will be tested.
+*/
+
+function otherAngle(a, b) {
+	return 180 - (a + b);
+  }
+
+console.log(otherAngle(90, 35));
+
+/*
+In this Kata you should fix/create a program that returns the following values:
+- false/False if either a or b (or both) are not numbers
+- a % b plus b % a if both arguments are numbers
+You may assume the following:
+- If a and b are both numbers, neither of a or b will be 0.
+*/
+
+
+const myFirstKata = (a, b) => {
+	if (typeof(a) !== 'number' || typeof(b) !== 'number') {
+	  return false;
+	 }
+	 else {
+	  return (a % b) + (b % a);
+	}
+}
+
+console.log(myFirstKata(2, 7));
